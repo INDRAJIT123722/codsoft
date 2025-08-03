72d07c7c599a50b8d67c7968a1525c43f53a385c
indrajit barui <indrajitbarui67@gmail.com>
2025-08-03 12:17:36 +0530

    added Task 1 -number guessing game

---
+++ Task 1/number_Guessing_game/Number_Guessing_game.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<windows.h>
+using namespace std;
+int main(){
+    int random,guess;
+    srand(time(0));
+    random=(rand()%100)+1;
+    int count=0;
+    cout<<"Welcome to the  World of Gussing number"<<endl;
+    do
+    {
+       cout<<"Enter your guess between(1 to 100): ";
+       cin>>guess;
+       count++;
+       if (guess<random)
+       {
+        cout<<"Guess a larger  number"<<endl;
+       }
+       else if (guess>random)
+       {
+         cout<<"Guess a smaller number: "<<endl;
+       }
+       else
+       {
+        cout<<"congratulation!! You have successfully guessed The number in :"<<count<<" attempts"<<endl;
+       }
+       
+       
+    } while (guess!=random);
+    cout<<"Thanks for playing: "<<endl;
+    cout<<"Developed by Indrajit barui: "; 
+}
---
+++ Task 1/number_Guessing_game/README.md
@@ -0,0 +1,5 @@
+#task 1-number guessing Game
+
+##this is console based number guessing game Developed in c++ for the Codsoft intrernship.
+
+the game generates a random number between 1 to 100.the user keep guessing until guess correct number.
